73ccb5c49c2af926809fd321d0ed6c68ed632c78
Andrzej Raczkowski <araczkowski@gmail.com>
2015-06-01 15:48:19 +0200

    Update acl.sql

--- plsql/acl.sql
+++ plsql/acl.sql
@@ -1,13 +1,13 @@
 BEGIN
-  DBMS_NETWORK_ACL_ADMIN.create_acl(acl         => 'jsreport_base_acl_pcs_owner.xml'
+  DBMS_NETWORK_ACL_ADMIN.create_acl(acl         => 'jsreport_base_acl_SVIETE.xml'
                                    ,description => 'An ACL for the jsreportopenshift-cwms.rhcloud.com website'
-                                   ,principal   => 'PCS_OWNER'
+                                   ,principal   => 'SVIETE'
                                    ,is_grant    => TRUE
                                    ,privilege   => 'connect'
                                    ,start_date  => SYSTIMESTAMP
                                    ,end_date    => NULL);
 
-  DBMS_NETWORK_ACL_ADMIN.assign_acl(acl        => 'jsreport_base_acl_pcs_owner.xml'
+  DBMS_NETWORK_ACL_ADMIN.assign_acl(acl        => 'jsreport_base_acl_SVIETE.xml'
                                    ,host       => 'jsreportopenshift-cwms.rhcloud.com'
                                    ,lower_port => 80
                                    ,upper_port => 80);
